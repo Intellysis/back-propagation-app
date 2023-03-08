0e7ac48384df41aa00ecbb948979107db0198ac2
Kerr L. Labajo <kerr.labajo@cit.edu>
2023-03-08 11:39:10 +0800

    Implement button functions

--- SupervisedLearning/BackPropagation/Form1.cs
+++ SupervisedLearning/BackPropagation/Form1.cs
@@ -7,14 +7,80 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Backprop;
 
 namespace BackPropagation
 {
     public partial class Form1 : Form
     {
+        NeuralNet nn;
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //2 input
+            //3 hidden
+            //1 output
+            nn = new NeuralNet(2,10,1);
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            for(int i = 0; i < Convert.ToInt32(textBox4.Text); i++)
+            {
+                //pos is neuron number at 0
+                //data should be from textBox1 as an input and is set as 1 for sample
+                //AND operations
+                nn.setInputs(0, 1.0);
+                nn.setInputs(0, 1.0);
+                nn.setDesiredOutput(0, 1.0);
+                nn.learn();
+
+                nn.setInputs(0, 1.0);
+                nn.setInputs(1, 0.0);
+                nn.setDesiredOutput(0, 0.0);
+                nn.learn();
+
+                nn.setInputs(0, 0.0);
+                nn.setInputs(1, 1.0);
+                nn.setDesiredOutput(0, 0.0);
+                nn.learn();
+
+                nn.setInputs(0, 0.0);
+                nn.setInputs(1, 0.0);
+                nn.setDesiredOutput(0, 0.0);
+                nn.learn();
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            nn.setInputs(0, Convert.ToDouble(textBox1.Text));
+            nn.setInputs(1, Convert.ToDouble(textBox2.Text));
+            nn.run();
+            textBox3.Text = "" + nn.getOuputData(0);
+        }
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+
         }
     }
 }
